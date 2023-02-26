1462267e014760b888d2e6595f6ecd29dbee183d
Vincent Chendrawan <vchendrawan@gmail.com>
2023-02-26 11:49:46 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,21 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+// Initialize and add the map
+function initMap() {
+    // The location of Berkeley 37.871666, -122.272781
+    const berkeley = { lat: 37.871, lng: -122.272 };
+    // The map, centered at Berkeley
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: berkeley,
+    });
+    // The marker, positioned at berkeley
+    const marker = new google.maps.Marker({
+      position: berkeley,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
